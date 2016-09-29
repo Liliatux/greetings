5f074f2e2996d581b7c8e19ace0dec70036a1a9c
Lilia <geertszelia@gmail.com>
2016-09-29 16:54:12 +0200

    Ajout d'un petit formulaire en HTML et jQuery avec modif du hello

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log('have fun !');
+
+var form = {
+	first_name: " ",
+	last_name: " ",
+	city: " "
+}
+
+function hello() {
+	$("#button").click(function() {
+		form["first_name"] = $("#first_name").val();
+		form["last_name"] = $("#last_name").val();
+		form["city"] = $("#city").val();
+		$("#username").text(form["first_name"] + form["last_name"]);
+		console.log(form);
+	});
+}
+
+hello();
